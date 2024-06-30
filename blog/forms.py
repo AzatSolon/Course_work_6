f4366c3e5a6fc73a7f4484ebb786c39789013a8d
Yamete Kudasai <ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHz9UP48vsX5QHtRbnWSJfttNWrsUcnSHPgtuXt7kPlq your_email@example.com -ssh>
2024-06-30 11:56:04 +0300

    rework course_6.4.0

--- blog/forms.py
+++ blog/forms.py
@@ -3,11 +3,11 @@ from mail.forms import StyleMixin
 from blog.models import Blog
 
 
-class PostForm(StyleMixin, ModelForm):
+class BlogForm(StyleMixin, ModelForm):
     """
     Форма создания статьи
     """
 
     class Meta:
         model = Blog
-        exclude = ("is_published", "views_count")
+        exclude = ['slug', 'views_count']
